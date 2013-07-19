1dac28e9d3542d3b767e191abcf16c9fa0ec01d8
Steve6760 <2.steve.schwartz@gmail.com>
2013-07-18 20:05:34 -0400

    update home page first time

--- MvcApplication1/MvcApplication1/Controllers/HomeController.cs
+++ MvcApplication1/MvcApplication1/Controllers/HomeController.cs
@@ -10,21 +10,21 @@ namespace MvcApplication1.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            ViewBag.Message = "Steve App to support claim mgmt program for Touchpoint.";
 
             return View();
         }
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            ViewBag.Message = "Claim App for Touchpoint";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Submit your claim here";
 
             return View();
         }
